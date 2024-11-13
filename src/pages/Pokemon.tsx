48a3f706c3a0d8e597c112a6d8e13751b6873d50
BrandonJulio <bjulio@unicesar.edu.co>
2024-11-13 16:19:57 -0500

    Consume la API y ya se hizo la parte del menú, y ya muestra todo los detalles de cada pokemón

--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -1,8 +1,58 @@
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import PokeballImg from '../assets/pokeball.png'
+import Footer from '../components/Footer'
+import style from './pokemon.module.css'
+// import Bulbasaur from '../assets/bulbasaur.gif'
+import { PokemonDetails } from '../types/types'
+import { useState, useEffect } from 'react'
+import { fecthPokemon } from '../api/fecthPokemon'
+import LoadingScreen from '../components/LoadingScreen'
+import { waitFor } from '../utils/utils'
 
 const Pokemon = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [pokemon, setPokemon] = useState<PokemonDetails>()
   const { name } = useParams()
-  return <div>{name}</div>
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    async function getPokemon () {
+      setIsLoading(true)
+      await waitFor(500)
+      const fecthedPokemon = await fecthPokemon(name as string)
+      setPokemon(fecthedPokemon)
+      setIsLoading(false)
+    }
+    getPokemon()
+  }, [name])
+
+  if (isLoading) {
+    return <LoadingScreen />
+  }
+  return (
+    <>
+      <button className={style.pokeballButton} onClick={() => navigate(-1)}>
+        <img className={style.pokeballImg} src={PokeballImg} alt='Pokeball' /> Go Back
+      </button>
+      <div className={style.pokemon}>
+        <main className={style.pokemonInfo}>
+          <div className={style.pokemonTitle}>
+            {pokemon?.name?.toUpperCase()}
+          </div>
+          <div>Nr. {pokemon?.id}</div>
+          <div><img
+            className={style.pokemonInfoImg}
+            src={pokemon?.imgSrc} alt={pokemon?.name}
+               />
+          </div>
+          <div>HP: {pokemon?.hp}</div>
+          <div>Attack: {pokemon?.attack}</div>
+          <div>Defendese: {pokemon?.defense}</div>
+        </main>
+      </div>
+      <Footer />
+    </>
+  )
 }
 
 export default Pokemon
